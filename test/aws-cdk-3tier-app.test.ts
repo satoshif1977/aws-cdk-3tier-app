e98e06238b3b3387960c642ed2a24d4fa82b357f
さとっぺ <satoshif1977@gmail.com>
2026-04-16 11:40:23 +0900

    Initial commit

---
+++ test/aws-cdk-3tier-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdk3TierApp from '../lib/aws-cdk-3tier-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-3tier-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdk3TierApp.AwsCdk3TierAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
